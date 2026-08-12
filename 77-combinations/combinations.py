42950685e3081d6ab6691c6b98deadcb133e0a3a
sairam-05 <sainaidukonidela143@gmail.com>
2026-08-12 13:42:19 +0530

    Time: 115 ms (33.72%) | Memory: 61.5 MB (10.10%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,9 +1,18 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        a=[]
-        for i in range(1,n+1):
-            a.append(i)
-        l=combinations(a,k)
-        return list(l)
+        res=[]
+        sol=[]
+        def back(start):
+            if len(sol)==k:
+                res.append(sol[::])
+                return
+            for i in range(start,n+1):
+                sol.append(i)
+                back(i+1)
+                sol.pop()
+        back(1)
+        return res
+
+
         
