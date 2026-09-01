c1dad8e339d423c920f2280292fbd849670c69d8
sairam-05 <sainaidukonidela143@gmail.com>
2026-09-01 14:09:33 +0530

    Time: 4 ms (2.12%) | Memory: 19.3 MB (19.51%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -1,10 +1,9 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         l=[]
+        re=0
         for i in range(n):
-            l.append(start)
-            start=start+2
-        res=l[0]
-        for i in range(1,len(l)):
-            res^=l[i]
-        return res
+            l.append(start+2*i)
+        for i in l:
+            re^=i
+        return re
