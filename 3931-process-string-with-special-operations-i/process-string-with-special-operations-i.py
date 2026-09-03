0e7b6bb51fdf7099f602716a6e636c31ca7b676d
sairam-05 <sainaidukonidela143@gmail.com>
2026-09-03 13:37:38 +0530

    Time: 172 ms (5.09%) | Memory: 32.6 MB (5.23%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res=[]
+
+        for i in s:
+            i.lower()
+            if i.isalpha():
+                res.append(i)
+                print(res)
+            elif i=="*" and len(res)>=1:
+                res.pop()
+            elif i=="#":
+                res+=res
+            elif i=="%":
+                res.reverse()
+                
+        s=""
+        for i in res:
+            s+=i
+        return s
