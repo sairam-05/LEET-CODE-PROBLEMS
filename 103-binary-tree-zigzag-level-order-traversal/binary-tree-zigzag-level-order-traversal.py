d66d6b3c726edfe1ff4efad87a3e63ab356676fa
sairam-05 <sainaidukonidela143@gmail.com>
2026-09-17 16:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (50.94%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -4,29 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def _init_(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result = []
-        if not root:
-            return []
-        queue = deque([root])
-        level = 0
-        while queue:
-            levelSize = len(queue)
-            level_node = []
-            level += 1
-            for i in range(0 , levelSize):
-                current = queue.popleft()
-                level_node.append(current.val)
-                if current.left:
-                    queue.append(current.left)
-                    
-                if current.right:
-                    queue.append(current.right)
-            
-            if level % 2 == 0:
-                level_node = level_node[::-1]
-
-            result.append(level_node)
-        return result
-        
+    def zigzagLevelOrder(self, root: TreeNode | None) -> list[list[int]]:
+        direction=1
+        deq=collections.deque([root])
+        res=[]
+        while deq:
+            level=[]
+            for i in range(len(deq)):
+                node=deq.popleft()
+                if node:
+                    level.append(node.val)
+                    deq.append(node.left)
+                    deq.append(node.right)
+            if level:
+                if direction %2==0:
+                    res.append(level[::-1])
+                else:
+                    res.append(level)
+                direction +=1
+        return res
